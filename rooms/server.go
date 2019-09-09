0be4d66cf0f378df6edc7d714cb373471a5084a8
Andrey Dyatlov <adyatlov@mesosphere.io>
2019-09-09 09:07:18 +0200

    make server ready

--- rooms/server.go
+++ rooms/server.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	exposer := &wsexposer.Exposer{Port: 8080}
+	exposer := &wsexposer.Exposer{Port: 8080, Host: "0.0.0.0"}
 	conveyor := simpleconveyor.NewConveyor()
 	err := exposer.Expose(conveyor)
 	if err != nil {
